af29dd3b290bf6c372f2ff7edcc580d0ac350c49
onelValery <monelvalery@gmail.com>
2023-05-25 15:37:20 -0400

    Tic-tac-toe possible Game Generator with symetric check

---
+++ StudentInfo.java
@@ -0,0 +1,29 @@
+/**
+* Contains a method <code>void display()</code> that all the <code>main</code>
+* methods call to show the student information. Fill the box with your personal
+* information.
+*
+* @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+*/
+
+public class StudentInfo {
+
+    /**
+    * Displays the student information: student name, id, section, etc for each
+    * member of the team.
+    */
+
+    public static void display() {
+
+        System.out.println("************************************************************");
+        System.out.println("*   Etudiant 1:                                Etudiant 2  *");
+        System.out.println("*   Name: Onel Valery Mezil             Meryem Benjelloun  *");
+        System.out.println("*   No Etudiant: 300260630          No Etudiant:300220488  *");
+        System.out.println("*   Section: A00                              Section A00  *");
+        System.out.println("************************************************************");
+        System.out.println();
+
+    }
+
+}
+
